4dcebe72de075ff3c60258614c1b70f24304540e
ElSeeR <sergio9garcia9@gmail.com>
2020-11-09 09:52:50 +0100

    commit 2

--- Tortilla.java
+++ Tortilla.java
@@ -1,33 +1,39 @@
 public class Tortilla{
 
     private String puntoCoccion;
-    
+
     private int numeroHuevos;
-    
+
     private boolean llevaCebolla;
-    
-    
-    public Tortilla(){
-       
+
+    public Tortilla(String puntoDeCoccion, int numeroDeHuevos){
+        puntoCoccion = puntoDeCoccion;
+        numeroDeHuevos = numeroHuevos;
+        llevaCebolla = false;
     }
-    
-    public int añadirHuevos(int numeroDeHuevos){
-        numeroHuevos = numeroDeHuevos;
-        return numeroHuevos;
+
+    public void añadirHuevos(int huevos){
+        numeroHuevos = huevos;
+        numeroHuevos = numeroHuevos + huevos;
     }
-    
-    public void puntoDeCoccion(String puntoCoccion){
-        
+
+    public void setPuntoDeCoccion(String coccion){
+        puntoCoccion = coccion;
     }
-    
-    public void llevaCebolla(boolean tieneCebolla){
+
+    public void llevaCebolla (boolean tieneCebolla){
         llevaCebolla = tieneCebolla;
-            if(tieneCebolla == true){
-                System.out.println("Ahora hay cebolla en la tortilla");
-            }
-            
-            else{
-                System.out.println("Vaya... con cebolla esta mejor, pero bueno, tu tortilla no llevará cebolla");
-            }
+        if(tieneCebolla == true){
+            System.out.println("Ahora hay cebolla en la tortilla");
+        }
+
+        else{
+            System.out.println("Vaya... con cebolla esta mejor, pero bueno, tu tortilla no llevará cebolla");
+        }
+    }
+    
+    public void imprimirEstadoTortilla(){
+        System.out.println("Hay " + numeroHuevos + " huevos, el punto de cocción es " +
+        puntoCoccion + ". Lleva cebolla?: " + llevaCebolla);
     }
 }
